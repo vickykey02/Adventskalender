b036a258a060975bb2f831f8f8f2e2e3e7b5e5bf
Victoria Kocks <148390293+vickykey02@users.noreply.github.com>
2024-11-27 16:36:15 +0100

    Update app.js
    Änderung Datum zum Test

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const days = {
 	"2024-12-10": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag11.png",
 	"2024-12-11": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag12.png",
 	"2024-12-12": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag13.png",
-    	"2024-12-13": "https://github.com/vickykey02/Adventskalender/raw/main/assets/Tag14.png", 
+    	"2024-11-13": "https://github.com/vickykey02/Adventskalender/raw/main/assets/Tag14.png", 
 	"2024-12-14": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag15.png",
 	"2024-12-15": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag16.png",
 	"2024-12-16": "C:/Users/vicky/Documents/Projekte/Adventskalender/assets/Tag17.png",
